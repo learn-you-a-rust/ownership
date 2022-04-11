813fca314bb4df973a4722a71112c26daf3ee899
greyspectrum <hello@greyspectrum.io>
2022-04-11 19:29:51 -0400

    ownership

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
 fn main() {
     let s = String::from("hello"); // s comes into scope
 
-    takes_ownership(s);            // s's value moves into the function
-                                   // ... so it's no longer valid here
+    takes_ownership(s);            // s's value moves into the function...
+                                   // ...and so is no longer valid here
+    let x = 5;                     // x comes in to scope
 
-    let x = 5;                     // x comes into scope
-    
-    makes_copy(x);                 // x would move into the function,
-                                   // but i32 is Copy, so it's okay to 
-                                   // still use x afterward
-
-    //let _t = s;                  // this doesn't work
-    let _y = x;                    // but this does work
-
-} // s and x would go out of scope, but s is already out of scope and x
-  // is on the stack, so it doesn't matter.
+    makes_copy(x);                 // x moves into the function, but
+                                   // but i32 is Copy, so it's ok to still
+                                   // use x afterward
+}
 
 fn takes_ownership(some_string: String) { // some_string comes into scope
     println!("{}", some_string);
-} // some_string goes out of scope and 'drop' is called, freeing the memory
+} // Here, some_string goes out of scope and "drop" is called, the
+  // underlying memory is freed
 
-fn makes_copy(some_int: i32) { // some_int comes into scope
-    println!("{}", some_int);
-} // some_int goes out of scope, but since it was on the stack, no memory
-  // needs to be freed
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope, since there was no heap
+  // allocation, nothing is freed either
